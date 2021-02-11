1423d4776a6dc2812b590b23e818e9841bc65743
hitohito1023 <kinaseryota@kinazeryoutanoMacBook-Pro.local>
2021-02-11 15:42:15 +0900

    bikeの描写完成

--- js/app.js
+++ js/app.js
@@ -21,19 +21,35 @@ var noise = x => {
 var player = new function() {
   this.x = c.width / 2;
   this.y = 0;
+  this.ySpeed = 0;
   this.rot = 0;
 
   this.img = new Image();
   this.img.src = "images/moto.png"
 
   this.draw = function() {
-    ctx.drawImage()
+    var p1 = c.height - noise(t + this.x) * 0.25;
+
+    if(p1 - 30 > this.y) {
+      this.ySpeed -= 0.1;
+    } else {
+      this.y = p1 - 30;
+      this.ySpeed = 0;
+    }
+
+    this.y -= this.ySpeed;
+
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.drawImage(this.img, 0, 0, 30, 30);
+
+    ctx.restore();
   }
 }
 var t = 0;
 
 function loop() {
-  t += 1;
+  t += 5;
   ctx.fillStyle = "#19f";
   ctx.fillRect(0, 0, c.width, c.height);
 
@@ -49,6 +65,8 @@ function loop() {
   ctx.lineTo(c.width, c.height);
 
   ctx.fill();
+
+  player.draw();
   requestAnimationFrame(loop);
 }
 
