681fd4fa2743f78e3676198fda97831742e6779b
hitohito1023 <kinaseryota@kinazeryoutanoMacBook-Pro.local>
2021-02-11 16:24:00 +0900

    finish

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var c = document.createElement("canvas");
 var ctx = c.getContext("2d");
-c.width = 600;
+c.width = 1000
 c.height = 400;
+
+var size = 15;
 
 document.body.appendChild(c);
 
@@ -11,7 +13,8 @@ while (perm.length < 255) {
   while (perm.includes(val = Math.floor(Math.random() * 255)));
   perm.push(val);
 }
-var lerp = (a, b, t) => a + (b -a) * (1 - Math.cos(t * Math.PI)) / 2;
+
+var lerp = (a, b, t) => a + (b - a) * (1 - Math.cos(t * Math.PI)) / 2;
 
 var noise = x => {
   x = x * 0.01 % 255;
@@ -26,52 +29,65 @@ var player = new function() {
   this.rSpeed = 0;
 
   this.img = new Image();
-  this.img.src = "images/moto.png"
+  this.img.src = "images/moto.png";
 
   this.draw = function() {
     var p1 = c.height - noise(t + this.x) * 0.25;
     var p2 = c.height - noise(t + 5 + this.x) * 0.25;
 
-    var grounded = 0;
+    var grounded = 0
 
-    if(p1 - 30 > this.y) {
+    if(p1 - size > this.y) {
       this.ySpeed += 0.1;
     } else {
-      this.y = p1 - 30;
-      this.ySpeed -= this.y - (p1 - 30.1);
+      this.ySpeed -= this.y - (p1 - size);
+      this.y = p1 - size;
 
       grounded = 1;
     }
 
-    var angle = Math.atan2((p2 -30) - this.y, (this.x + 5) - this.x);
+    if(!playing || grounded && Math.abs(this.rot) > Math.PI * 0.5) {
+      playing = false;
+      this.rSpeed = 5;
+      k.ArrowUp = 1;
+      this.x -= speed * 5;
+    }
 
-    // this.rot = angle;
+    var angle = Math.atan2((p2 - size) - this.y, (this.x + 5) - this.x);
+
 
     this.y += this.ySpeed;
 
-    if(grounded) {
+    if(grounded && playing) {
       this.rot -= (this.rot - angle) * 0.5;
       this.rSpeed = this.rSpeed - (angle - this.rot);
     }
 
-    this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.5;
+    this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.05;
     this.rot -= this.rSpeed * 0.1;
+
+    if(this.rot > Math.PI) this.rot = -Math.PI;
+    if(this.rot < -Math.PI) this.rot = Math.PI;
+
 
     ctx.save();
     ctx.translate(this.x, this.y);
     ctx.rotate(this.rot);
-    ctx.drawImage(this.img, 0, 0, 30, 30);
+    ctx.drawImage(this.img, -size, -size, 30, 30);
 
     ctx.restore();
   }
 }
+
 var t = 0;
 var speed = 0;
+var playing = true;
 var k = {ArrowUp: 0, ArrowDown: 0, ArrowLeft: 0, ArrowRight: 0};
+
 
 function loop() {
   speed -= (speed - (k.ArrowUp - k.ArrowDown)) * 0.1;
-  t += 5 * speed;
+  t += 10 * speed;
   ctx.fillStyle = "#19f";
   ctx.fillRect(0, 0, c.width, c.height);
 
